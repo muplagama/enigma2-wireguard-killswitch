3bcdb15bc72902bad07e4b796d1d974eb46c2d30
root <LouCipher@segfault.net>
2023-01-25 14:11:15 +0000

    Wed Jan 25 14:11:15 GMT 2023

--- killswitch.sh
+++ killswitch.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 ### Save default route ###
-if [ -n /etc/wireguard/route_default ]; then
-   ip route show default | grep default > /etc/wireguard/route_default
-fi
+save_default=$(grep default /etc/wireguard/route_default | grep default )
+        if  [ -z "$save_default" ]
+        then
+        ip route show default | grep default > /etc/wireguard/route_default
+        else
+        echo "default already set"
+        fi
+
 ### Grep VPN url ###
-url_grep=$(cat /etc/wireguard/wg0.conf | grep Endpoint | awk -F'=' '{print $2}' | awk -F# '{gsub(/ /,"");print ($1) }' |awk -F ':' '{print $1}')
+url_grep=$(grep Endpoint /etc/wireguard/wg0.conf | awk -F'=' '{print $2}' | awk -F# '{gsub(/ /,"");print ($1) }' | awk -F ':' '{print $1}')
 
 ### Sets ###
 dns_ip=1.1.1.1
-route_default=$(cat /etc/wireguard/route_default | grep default)
-route_ip=$(cat /etc/wireguard/route_default | awk '/default/ {print $3}')
-route_dev=$(cat /etc/wireguard/route_default | awk '/default/ {print $5}')
+route_default=$(grep default /etc/wireguard/route_default | grep default)
+route_ip=$(grep default /etc/wireguard/route_default | awk '/default/ {print $3}')
+route_dev=$(grep default /etc/wireguard/route_default | awk '/default/ {print $5}')
 
 while true
 do
@@ -22,18 +27,18 @@ do
         add_default=$(ip route show default | grep default)
                 if  [ -z "$add_default" ]
                 then
-                ip route add $route_default
+                ip route add default via "$route_ip" dev "$route_dev"
                 fi
 ### check wg ping
-        ping_check=$(ping -c 2 -I wg0 1.1.1.1 | grep time)
-                if  [ ! -z "$ping_check" ]
+        ping_check=$(ping -c 2 -I wg0 "$dns_ip" | grep time)
+                if  [ -n "$ping_check" ]
                 then
 ### lookup srv ip
-        dns_v4=$(nslookup -query=A $url_grep 1.1.1.1 | grep Address | sed '/:53$/d' | sed s/^[^0-9]*//)
+        dns_v4=$(nslookup -query=A "$url_grep" "$dns_ip" | grep Address | sed '/:53$/d' | sed s/^[^0-9]*//)
         rem_backup_v4=$(ip route show | grep "$dns_v4" )
                 if  [ -n "$rem_backup_v4" ]
                 then
-                ip route del $dns_v4
+                ip route del "$dns_v4"
                 fi
                 else
                 echo "Ping not  OK"
@@ -52,16 +57,16 @@ do
         dns_request=$(ip route show | grep "default")
                 if  [ -z "$dns_request" ]
                 then
-                ip route add $dns_ip via $route_ip dev $route_dev
+                ip route add "$dns_ip" via "$route_ip" dev "$route_dev"
                 sleep 1
-        dns_v4=$(nslookup -query=A $url_grep 1.1.1.1 | grep Address | sed '/:53$/d' | sed s/^[^0-9]*//)
-                ip route del $dns_ip
+        dns_v4=$(nslookup -query=A "$url_grep" "$dns_ip" | grep Address | sed '/:53$/d' | sed s/^[^0-9]*//)
+                ip route del "$dns_ip"
                 fi
 ### add Backup Route
         add_backup_A=$(ip route show | grep "$dns_v4")
                 if  [ -z "$add_backup_A" ]
                 then
-                ip route add $dns_v4 via $route_ip dev $route_dev
+                ip route add "$dns_v4" via "$route_ip" dev "$route_dev"
                 fi
                 wg-quick down wg0 > /dev/null 2>&1
                 wg-quick up wg0
@@ -69,3 +74,4 @@ do
                 fi
 
 done
+
